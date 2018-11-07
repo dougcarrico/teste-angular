db093fa51b6c13ff08b24b0612da71b180fb9f59
Douglas Carriço <douggc@gmail.com>
2018-11-07 16:11:58 -0200

    Event Binding

--- src/app/cursos/curso-detalhe/curso-detalhe.component.ts
+++ src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
-  styleUrls: ['./curso-detalhe.component.sass']
+  styleUrls: ['./curso-detalhe.component.scss']
 })
 export class CursoDetalheComponent implements OnInit {
 
--- src/app/data-brinding/data-brinding.component.ts
+++ src/app/data-brinding/data-brinding.component.ts
@@ -3,15 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-brinding',
   templateUrl: './data-brinding.component.html',
-  styleUrls: ['./data-brinding.component.sass']
+  styleUrls: ['./data-brinding.component.scss']
 })
 export class DataBrindingComponent implements OnInit {
 
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem = 'https://via.placeholder.com/150';
-
-
+  value = 0;
+  letra: string;
+  valorSalvo:string;
+  isMouseOver: boolean = false;
 
   getValor(){
     return 3;
@@ -19,6 +21,23 @@ export class DataBrindingComponent implements OnInit {
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('botão clicado!')
+  }
+
+  apertado(evento: KeyboardEvent){
+    console.log(evento.key);
+    this.letra = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { 
